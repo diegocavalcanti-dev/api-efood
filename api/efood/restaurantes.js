8106b9fc951dad5d650c3ce61c0d9eef6915c5ee
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-08-06 09:07:03 -0300

    fix js

--- api/efood/restaurantes.js
+++ api/efood/restaurantes.js
@@ -1,10 +1,14 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'api', 'efood', 'restaurantes.json');
-  const fileData = fs.readFileSync(filePath, 'utf8');
-  const data = JSON.parse(fileData);
+  try {
+    const filePath = path.join(process.cwd(), 'api', 'efood', 'restaurantes.json')
+    const fileData = fs.readFileSync(filePath, 'utf8')
+    const data = JSON.parse(fileData)
 
-  res.status(200).json(data);
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao carregar os restaurantes.' })
+  }
 }
--- api/efood/restaurantes/[id].js
+++ api/efood/restaurantes/[id].js
@@ -6,15 +6,19 @@ export default function handler(req, res) {
     query: { id }
   } = req
 
-  const filePath = path.join(process.cwd(), 'api', 'efood', 'restaurantes.json')
-  const jsonData = fs.readFileSync(filePath, 'utf8')
-  const restaurantes = JSON.parse(jsonData)
+  try {
+    const filePath = path.join(process.cwd(), 'api', 'efood', 'restaurantes.json')
+    const jsonData = fs.readFileSync(filePath, 'utf8')
+    const restaurantes = JSON.parse(jsonData)
 
-  const restaurante = restaurantes.find(r => r.id === Number(id))
+    const restaurante = restaurantes.find(r => r.id === Number(id))
 
-  if (!restaurante) {
-    res.status(404).json({ error: 'Restaurante não encontrado' })
-  } else {
-    res.status(200).json(restaurante)
+    if (!restaurante) {
+      res.status(404).json({ error: 'Restaurante não encontrado' })
+    } else {
+      res.status(200).json(restaurante)
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao carregar o restaurante.' })
   }
 }
